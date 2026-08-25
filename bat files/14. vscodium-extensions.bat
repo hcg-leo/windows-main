ad8e3411a2b6c4e5a0afd9eff7db46434a1a70b6
hcg-leo <aran20111118@gmail.com>
2026-08-26 00:39:41 +0100

    removes ps1 file and added extension

--- bat files/14. vscodium-extensions.bat
+++ bat files/14. vscodium-extensions.bat
@@ -4,6 +4,7 @@ echo installing VS codium extensions...
 call codium --install-extension arcticicestudio.nord-visual-studio-code
 call codium --install-extension vscodevim.vim
 call codium --install-extension esbenp.prettier-vscode
+call codium --install-extension nur-srijan.markdown-rich-preview
 
 echo all extensions installed!
 pause
